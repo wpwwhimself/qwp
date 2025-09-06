bb71a6b5cbbabf47cc81d1cb6df97e74219526da
Wojciech Przybyła <contact@wpww.pl>
2025-09-06 17:09:02 +0200

    ⚓ added shipyard's schedule

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+require __DIR__.'/Shipyard/shipyard_schedule.php';
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
