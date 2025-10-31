572ffa6520e812f1a8c5b523f4eecfed1e97307b
Wojciech Przybyła <contact@wpww.pl>
2025-10-31 21:39:21 +0100

    ⚓ updated theme

---
+++ app/ShipyardTheme.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App;
+
+class ShipyardTheme
+{
+    #region theme
+    /**
+     * Available themes:
+     * - origin - separated cells, clean background, contents floating in the middle
+     * - austerity - broad background, main sections spread out
+     */
+    public const THEME = "future";
+    #endregion
+
+    #region colors
+    /**
+     * App accent colors:
+     * - primary - for background, primary (disruptive) actions and important text
+     * - secondary - for default buttons and links
+     * - tertiary - for non-disruptive interactive elements
+     */
+    public const COLORS = [
+        "primary" => "#17ecb0",
+        "secondary" => "#1d9271",
+        "tertiary" => "#2cbcff",
+    ];
+
+    public static function getColors(): array
+    {
+        return self::COLORS;
+    }
+
+    public static function getGhostColors(): array
+    {
+        return array_map(
+            fn ($clr) => $clr . "77",
+            self::COLORS
+        );
+    }
+    #endregion
+
+    #region fonts
+    /**
+     * type in the font declaration just as you would in a CSS file
+     */
+    public const FONTS = [
+        "base" => ["Raleway", "sans-serif"],
+        "heading" => ["Krona One", "sans-serif"],
+        "mono" => ["Ubuntu Mono", "monospace"],
+    ];
+
+    // if fonts come from Google Fonts, add the URL here
+    public const FONT_IMPORT_URL = 'https://fonts.googleapis.com/css2?family=Krona+One&family=Raleway:ital,wght@0,100..900;1,100..900&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap';
+    #endregion
+}
