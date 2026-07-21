3b3c4b33d7d1eba307d31b71de97e4449314f80c
Wojciech Przybyła <contact@wpww.pl>
2026-07-21 14:30:36 +0200

    ⚓ fixed references to views

--- resources/views/pages/runs/show.blade.php
+++ resources/views/pages/runs/show.blade.php
@@ -1,4 +1,4 @@
-@extends("layouts.shipyard.admin")
+@extends("shipyard::layouts.admin")
 @section("title", "Zarządzanie sesją")
 @section("subtitle", $run->task->name)
 
--- resources/views/pages/scopes/show.blade.php
+++ resources/views/pages/scopes/show.blade.php
@@ -1,4 +1,4 @@
-@extends("layouts.shipyard.admin")
+@extends("shipyard::layouts.admin")
 @section("title", $scope->name)
 @section("subtitle", $scope->project->name)
 
